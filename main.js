c913c3a1b0b961ce2ba3b826ea67dc1932f77489
asad3536 <asad.aziz@purelogics.net>
2023-02-23 13:48:30 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+
+  const servicesSlider = new Swiper('.services-slider', {
+    slidesPerView: 4,
+    spaceBetween: 12,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });

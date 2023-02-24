0c429a3cdde90b969abc86d904074746f7ccbe6e
asad3536 <asad.aziz@purelogics.net>
2023-02-24 21:07:30 +0500

    privacy policy page and contact us page ui design

--- main.js
+++ main.js
@@ -47,3 +47,29 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+
+
+  
+  const gallerySlider = new Swiper('.gallery-slider', {
+    slidesPerView: 3.5,
+    spaceBetween: 30,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });

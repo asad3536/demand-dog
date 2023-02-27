3e06b636af526520615a04173656d81d53d8bac8
asad3536 <asad.aziz@purelogics.net>
2023-02-27 20:26:26 +0500

    home page responsive view

--- main.js
+++ main.js
@@ -20,13 +20,28 @@ const swiper = new Swiper('.swiper', {
     scrollbar: {
       el: '.swiper-scrollbar',
     },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
   });
 
-
-
-  const servicesSlider = new Swiper('.services-slider', {
-    slidesPerView: 4,
-    spaceBetween: 30,
+  const featuresSlider = new Swiper('.features-slider', {
+    slidesPerView: 3,
+    spaceBetween: 47,
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -46,6 +61,89 @@ const swiper = new Swiper('.swiper', {
     scrollbar: {
       el: '.swiper-scrollbar',
     },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      700: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+
+      900:{
+        slidesPerView: 2,
+        spaceBetween: 30
+      }
+      ,
+
+      1000: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      }
+    }
+  });
+
+
+  const servicesSlider = new Swiper('.services-slider', {
+    slidesPerView: 4,
+    spaceBetween: 47,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      700: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+
+      900:{
+        slidesPerView: 2,
+        spaceBetween: 30
+      }
+      ,
+
+      1000: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      }
+    }
   });
 
 
